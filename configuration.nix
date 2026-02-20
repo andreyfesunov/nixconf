699f5c47626eab6bc2f3b02267ea6b27bfb054ce
andreyfesunov <fsnv04@gmail.com>
2026-02-20 22:43:04 +0300

    fix(deprecation): update gnome specific variables due to deprecation

--- configuration.nix
+++ configuration.nix
@@ -5,7 +5,8 @@
   pkgs,
   unstable,
   ...
-}: {
+}:
+{
   imports = [
     ./flatpak.nix
     ./home.nix
@@ -55,8 +56,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -180,6 +181,7 @@
     julia
     gnumake
     filezilla
+    tmux
     unstable.code-cursor
   ];
 
